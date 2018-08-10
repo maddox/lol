0f18dd193665d0aa9d6f3d165bd56271bbe0756a
Jon Maddox <maddox@github.com>
2018-08-10 11:13:35 -0400

    LOLOL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   console.log("sending an lol")
-  res.send("lulz!")
+  res.send("LOLOL!")
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
