804a9df80bc655a01594e33d2fdbfa90d226d44c
Jeff Rafter <jeffrafter@github.com>
2018-09-25 15:53:18 -0700

    Commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('noodle-it')
 const express = require('express')
 
+console.log("restart------ again!!!")
 const app = express()
 const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-  console.info("oh it did it")
-  res.send("blork!")
+  console.info("oh it sure did do it - together")
+  res.send("blannnnkiiiii!")
 })
 
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
+
