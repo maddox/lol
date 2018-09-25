998773692ee3856570a0cedc11c448f3c682a61d
Jon Maddox <maddox@github.com>
2018-09-25 14:39:49 -0400

    stuff

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   console.log("sending another lol here we go again")
-  res.send("it will work")
+  res.send("it will work?????")
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
